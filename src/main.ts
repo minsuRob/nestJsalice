f83b719c719feecc7a85087b6b7f184509a87a09
minsu.lee <minsu.rob@gmail.com>
2024-12-06 17:39:47 +0900

    feat: swagger 처리

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ConfigService } from "@nestjs/config";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  //swagger setup
+  const config = new DocumentBuilder()
+    .setTitle("Alice Lab Nest")
+    .setDescription("Alice TEST API")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api-docs", app, document);
+
   await app.listen(configService.get<number>("BACKEND_PORT"));
 }
 
--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateProductDto {
+  @ApiProperty({ description: "상품 이름", example: "iphone 16" })
   name: string;
+  @ApiProperty({ description: "상품 가격", example: 1000000 })
   price: number;
+  @ApiProperty({ description: "상품 설명", example: "so fasts" })
   desc: string;
+  @ApiProperty({ description: "상품 이미지", example: "https://..." })
   productImg: string;
 }
