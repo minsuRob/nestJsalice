76f3217463023cc9813400372ca3294bc7c59cdc
minsu.lee <minsu.rob@gmail.com>
2024-12-06 15:42:27 +0900

    fix: 일단커밋

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
+import { DatabaseModule } from './database/database.module';
+import { ProductModule } from './product/product.module';
 import * as Joi from "@hapi/joi";
 
 @Module({
@@ -11,6 +13,8 @@ import * as Joi from "@hapi/joi";
         BACKEND_PORT: Joi.number().required(),
       }),
     }),
+    DatabaseModule,
+    ProductModule,
   ],
   controllers: [AppController],
   providers: [AppService],
